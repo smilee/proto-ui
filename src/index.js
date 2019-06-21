6f618ea03d640ea8b9d8f914d7e5d3e23397f341
Seungmin Lee <seungmin@uxplus.dev>
2019-06-21 15:28:46 +0900

    Add wrapper
    To package components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-export default {}
+// Import vue components
+import ProtoButton from '../packages/button'
+
+// Organize imported components into an array
+const components = [
+  ProtoButton
+]
+
+// Declare install function executed by Vue.use()
+export function install (Vue) {
+  components.forEach(component => {
+    if (install.installed) return
+    install.installed = true
+    Vue.component(component.name, component)
+  })
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+  install
+}
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// To allow use as module (npm/webpack/etc.) export components
+export default { ...components }
